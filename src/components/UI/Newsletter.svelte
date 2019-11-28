3d2617b10fc496e9f99ed012851ba90ea46c2603
EtienneLareau <tiennelindien@gmail.com>
2019-11-27 22:58:45 -0500

    form

--- src/components/UI/Newsletter.svelte
+++ src/components/UI/Newsletter.svelte
@@ -60,7 +60,7 @@
         <h2 class="title">Vous ne voulez manquez aucun voyage ?</h2>
         <p class="para">Inscrivez-vous à notre infolettre pour ne rien manqué</p>
         
-        <form action="/" name="contactForm" method="POST" netlify>
+        <form action="/about" name="contactForm" method="POST" netlify>
             <label for="name">Nom et prénom</label>
                 <input type="text" id="name" name="names" placeholder="" required >
             
