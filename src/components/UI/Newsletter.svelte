e006087f4dfa1df8c2f414ec399e48e0fed242a7
EtienneLareau <tiennelindien@gmail.com>
2019-12-05 09:41:21 -0500

    contact

--- src/components/UI/Newsletter.svelte
+++ src/components/UI/Newsletter.svelte
@@ -2,8 +2,8 @@
     section {
         position: relative;
         width: 100% ;
-        height: 500px ;
-        background: #278e73 ; 
+        height:700px;
+         
     }
 
     label {
@@ -42,7 +42,7 @@
         display: block;
         height: 30px ;
         width: 75% ;
-        border: none;
+        border: 1px solid #278e73;
         cursor: pointer ;
         margin-bottom: 22px;
         color: red;
@@ -50,8 +50,39 @@
         padding: 8px 10px;
     }
 
-    
+    button {
+        background: #278e73;
+        color:white;
+        padding: 20px 40px;
+        border:none;
+        font-size:1.5rem;
+        box-shadow: 1px 1px 3px rgba(128, 128, 128, 0.425);
+        transition: all 0.25s ease-out;
+    }
 
+    button:focus, button:hover {
+        background: white;
+        color:#278e73;
+        box-shadow:none;
+    }
+
+    @media only screen and (max-width:725px) {
+        .block {
+        position: relative ;
+        max-width: 100%;
+        margin: 40px 20px 40px;
+    }
+
+        h2 {
+            font-size:1.9rem;
+            padding-right:20px;
+            text-align:left;
+        }
+        p {
+            text-align: left;
+            padding-right:20px;
+        }
+    }
     
 </style>
 
@@ -67,7 +98,7 @@
             <label for="email">Entrez votre adresse courriel</label>
                 <input type="email" id="email" name="email"  required/>
             
-            <button type="submit">S'inscrire</button>
+            <button type="submit">INSCRIVEZ-VOUS</button>
         </form>
     </div>
 </section>
