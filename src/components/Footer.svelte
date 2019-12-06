81bfdf302c052c7135075a3f2c2dab2de02c5f25
EtienneLareau <tiennelindien@gmail.com>
2019-12-06 12:32:53 -0500

    almost done

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,43 @@
 <style>
     footer {
-	background-color:#86fadb;
+	background-color: #f3fffc;
     height:400px;
     top:0;
 }
+    .gid {
+        width:90%;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-gap:2em;
+        margin: 0 auto;
+    }
+
+    .gid-cont {
+        background: palevioletred;
+        height:300px;
+    }
+
+    .gid-cont > h2,p {
+        margin: 20px 0 0 20px;
+    }
 </style>
 
 <footer>
+    <div class="foo">
+        <div class="gid">
+        <div class="gid-cont">
+            <h2>Menu</h2>
+            <p class="menu-link">Le voyage</p>
+            <p class="menu-link">Les cours</p>
+            <p class="menu-link">Le Spot</p>
+            <p class="menu-link">Les avantages</p>
+            <p class="menu-link">Je m'inscris</p>
+        </div>
+        <div class="gid-cont"></div>
+        <div class="gid-cont"></div>
+        
+        </div>
     
+    
+    </div>
 </footer>
